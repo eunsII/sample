07f1906e8709a1cd99ff8b7411f81a23e9e63050
y <y@DESKTOP-1E4JF9F>
2022-06-09 16:13:31 +0900

    2nd

--- sample/src/main/java/com/githrd/web/controller/Euns2Controller.java
+++ sample/src/main/java/com/githrd/web/controller/Euns2Controller.java
@@ -1,8 +1,10 @@
 package com.githrd.web.controller;
 
 import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/euns2")
 public class Euns2Controller {
 
 }
