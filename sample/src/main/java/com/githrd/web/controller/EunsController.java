27805cc9aacca27ee01e4f26f4e4ab007fe5eb22
y <y@DESKTOP-1E4JF9F>
2022-06-09 15:52:46 +0900

    1st commit

---
+++ sample/src/main/java/com/githrd/web/controller/EunsController.java
@@ -0,0 +1,10 @@
+package com.githrd.web.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/euns")
+public class EunsController {
+	
+}
